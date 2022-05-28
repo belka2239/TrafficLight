4f89634c53b6e26802c981a37a6f923128fd788c
belka2239 <belka2239@gmail.com>
2022-05-28 18:55:27 +0300

    Initial Commit

---
+++ TrafficLights/TrafficLightsApp.swift
@@ -0,0 +1,17 @@
+//
+//  TrafficLightsApp.swift
+//  TrafficLights
+//
+//  Created by VG on 28.05.2022.
+//
+
+import SwiftUI
+
+@main
+struct TrafficLightsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
